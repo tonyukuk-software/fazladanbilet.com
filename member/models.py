d0f1a71c4323d8b98f3441296c7f6590cbbc810a
cemkiy <se.cemkiy@gmail.com>
2014-10-23 10:07:23 +0300

    Migraitons  -update all models

--- member/models.py
+++ member/models.py
@@ -8,16 +8,25 @@ class Member(models.Model):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
     email = models.CharField(max_length=50, unique=True)
-    city = models.CharField(max_length=20)
+    point = models.PositiveIntegerField(default=0)
+    point_counter = models.PositiveIntegerField(default=0)#how many peoples give vote
     active = models.BooleanField(default=True, editable=False)
     cdate = models.DateTimeField(auto_now_add=True)
 
 class Wallet(models.Model):
     member = models.ForeignKey(Member)
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=20)
+
 class On_Sales(models.Model):
     member = models.ForeignKey(Member)
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
     total_ticket = models.PositiveIntegerField(default=0)
+    based_city = models.CharField(max_length=20)
+    image_url = models.URLField()
     amount_bitcoin = models.FloatField(default=0)
     cdate = models.DateTimeField(auto_now_add=True)
     edate = models.DateTimeField()
@@ -33,5 +42,13 @@ class Orders(models.Model):
     active = models.BooleanField(default=True, editable=False)
     cdate = models.DateTimeField(auto_now_add=True)
 
+class After_Sale(models.Model): #Feedback from shiping members
+    orders = models.ForeignKey(Orders)
+    status = models.PositiveIntegerField(default=0)
+    description = models.CharField(max_length=500)
+    cdate = models.DateField(auto_now_add=True)
+
+class City(models.Model):
+    city_name = models.CharField(max_length=10)
 
 
