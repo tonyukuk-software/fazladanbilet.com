20d940beaea66e8ea2a7b8125cc60d2613c3b246
cemkiy <se.cemkiy@gmail.com>
2014-10-28 09:01:35 +0200

    Admin Access  -member tables

--- jackal/admin.py
+++ jackal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.db.models import get_models, get_app
 # Register your models here.
+
+from member.models import *
+
+for model in get_models(get_app('member')):
+    admin.site.register(model)
