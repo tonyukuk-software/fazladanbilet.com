b80726b354da2016347218a4cde9ab9c6690a76c
cemkiy <se.cemkiy@gmail.com>
2014-11-28 23:30:04 +0200

    Bug-Fix already login user dilemma  -fixed: sign up added auto redirect  -fixed header auth problem(home,ticket_pool)

--- jackalprojects/views.py
+++ jackalprojects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from member.models import On_Sales
 
+
 __author__ = 'cemkiy'
 __author__ = 'barisariburnu'
 __author__ = 'kaykisizcom'
@@ -10,7 +11,7 @@ __author__ = 'kaykisizcom'
 
 
 def home_page(request):
-    return render_to_response('home_page.html', context_instance=RequestContext(request))
+    return render_to_response('home_page.html', locals(), context_instance=RequestContext(request))
 
 def ticket_pool(request):
     tickets = On_Sales.objects.filter(active=True)
