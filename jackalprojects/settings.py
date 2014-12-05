02097c2da27f1a18de493dcdf80364b83d334b78
Mehmet KAYKISIZ <m.kaykisiz@gmail.com>
2014-12-06 00:23:37 +0200

    OpenShift DB update
    OpenShift DB update

--- jackalprojects/settings.py
+++ jackalprojects/settings.py
@@ -63,9 +63,9 @@ WSGI_APPLICATION = 'jackalprojects.wsgi.application'
 DATABASES = {
    'default': {
           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-          'NAME': 'jackalproject',
-          'USER': 'adminjsuamws',
-          'PASSWORD': 'eY_Nu2mrcSsX',
+          'NAME': 'jackaldatabase',
+          'USER': 'adminic2zdsn',
+          'PASSWORD': 'wwg-xMWqlYQp',
           'HOST': 'localhost',
     }
 }
