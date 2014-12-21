9a6b6c75836dd4ddccfce04a8271baebdd9bf8bb
cemkiy <se.cemkiy@gmail.com>
2014-12-21 14:16:17 +0200

    JK-43 send mail  -added mail part

--- send_bitcoin.py
+++ send_bitcoin.py
@@ -1,9 +1,12 @@
+from django.template.loader import get_template
+from mailgun import mailgun
+
 __author__ = 'cemkiy'
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Unicode - Django
 
-from decimal import Decimal
+from decimal import Decimal, Context
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jackalprojects.settings")
 from member.models import Orders
@@ -22,6 +25,14 @@ def main():
             coinbase_operator.send_bitcoin(order.user_url_for_btc_send, amount)
         except Exception as e:
             print e
+        template = get_template("mail_send_bitcoin.html")
+        context = Context({'username': order.on_sales.member.username,
+                           'ticket_name': order.on_sales.title,
+                           'bitcoin_url': order.user_url_for_btc_send,
+                           'amount': amount}) #send amount
+        content = template.render(context)
+        mailgun_operator = mailgun()
+        mailgun_operator.send_mail_with_html(order.on_sales.member.email, content)
 
 
 if __name__ == "__main__":
