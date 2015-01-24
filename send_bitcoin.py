ceb9186b44d5c52cd2a24ff1ed8ff6362bca49f7
cemkiy <se.cemkiy@gmail.com>
2015-01-24 19:20:51 +0200

    send_bitcoin fix

--- send_bitcoin.py
+++ send_bitcoin.py
@@ -2,19 +2,16 @@
 # -*- coding: utf-8 -*-
 # Unicode - Django
 __author__ = 'cemkiy'
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jackalprojects.settings")
+from member.models import Orders
 from django.template.loader import get_template
 import decimal
 from bitcoin.coinbase_api import coinbase_api
 from mailgun import mailgun
 from django.template import Context
-
-
-
+from coinbase import CoinbaseAmount
 from decimal import Decimal
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jackalprojects.settings")
-from member.models import Orders
-
 
 def main():
     coinbase_operator = coinbase_api()
@@ -27,21 +24,22 @@ def main():
         print comission
         amount = amount - comission # without comission
         print amount
-        # try:
-        coinbase_operator.send_bitcoin(to_adress=order.user_url_for_btc_send, amount=amount, note='fazladanbilet sends your bitcoin')
-        template = get_template("mail_send_bitcoin.html")
-        context = Context({'username': order.on_sales.member.username,
-                           'ticket_name': order.on_sales.title,
-                           'bitcoin_url': order.user_url_for_btc_send,
-                           'amount': amount}) #send amount
-        content = template.render(context)
-        mailgun_operator = mailgun()
-        mailgun_operator.send_mail_with_html(order.on_sales.member.email, content)
-        # except Exception as e:
-        #     print e
-        #     mailgun_operator = mailgun()
-        #     text = str(order.id)+' '+str(order.on_sales.member.username)+' failure payment'
-        #     mailgun_operator.send_mail('info@fazladanbilet.com', text)
+        try:
+            c_amount = CoinbaseAmount(amount, 'BTC')
+            coinbase_operator.send_bitcoin(to_adress=order.user_url_for_btc_send, amount=c_amount, note='fazladanbilet sends your bitcoin')
+            template = get_template("mail_send_bitcoin.html")
+            context = Context({'username': order.on_sales.member.username,
+                               'ticket_name': order.on_sales.title,
+                               'bitcoin_url': order.user_url_for_btc_send,
+                               'amount': amount}) #send amount
+            content = template.render(context)
+            mailgun_operator = mailgun()
+            mailgun_operator.send_mail_with_html(order.on_sales.member.email, content)
+        except Exception as e:
+            print e
+            mailgun_operator = mailgun()
+            text = str(order.id)+' '+str(order.on_sales.member.username)+' failure payment'
+            mailgun_operator.send_mail('info@fazladanbilet.com', text)
 
 
 if __name__ == "__main__":
